fa53302450d607022a53baa1cb817c9f0dc3bc4b
sinon-waren <sinonwaren2000@gmail.com>
2024-06-02 22:56:09 +0200

    add of the addTask function in script and style addTask

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask () {
-    if(inputBox === ''){
-        alert("you must write something!")
+    if(inputBox.value === ""){
+        alert("you must write something!");
     }
     else{
         let li = document.createElement("li");
@@ -15,3 +15,12 @@ function addTask () {
     }
 
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toogle("checked")
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
