001683708ee99c4eee28ff10dfc85e09c7101c97
sinon-waren <sinonwaren2000@gmail.com>
2024-06-03 22:39:07 +0200

    fix error on the toggle function

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ function addTask () {
         span.innerHTML = "\u00d7";
         li.appendChild(span)   
     }
+    inputBox.value === ""
 
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList.toogle("checked")
+        e.target.classList.toggle("checked");
     }
     else if (e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
